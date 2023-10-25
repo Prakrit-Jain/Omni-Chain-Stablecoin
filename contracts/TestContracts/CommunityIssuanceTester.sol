06dea511dd4f5430f97d44707b2ebac36a110667
Prakrit-Jain <prakrit440@gmail.com>
2023-10-25 16:39:44 +0530

    refactor : removed GRVTToken and LockedGRVT contract

--- contracts/TestContracts/CommunityIssuanceTester.sol
+++ contracts/TestContracts/CommunityIssuanceTester.sol
@@ -6,10 +6,6 @@ import "../GRVT/CommunityIssuance.sol";
 
 contract CommunityIssuanceTester is CommunityIssuance {
 
-	function obtainGRVT(uint256 _amount) external {
-		grvtToken.transfer(msg.sender, _amount);
-	}
-
 	function getLastUpdateTokenDistribution() external view returns (uint256) {
 		return _getLastUpdateTokenDistribution();
 	}
